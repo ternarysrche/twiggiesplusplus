8e8915d92473f4f42eb767e52520c61d2338f306
Alex Li <alxli@mit.edu>
2024-09-15 00:11:10 -0400

    Add files via upload
    LATEST UPDATED VERSION

---
+++ new_midi.py
@@ -0,0 +1,176 @@
+# New version of the hand tracking module
+import cv2
+import mediapipe as mp
+import time 
+
+import tinysoundfont
+import time
+import threading
+
+synth = tinysoundfont.Synth()
+sfid = synth.sfload("/Users/alxli/Documents/CS Projects/2024 HackMIT/florestan-subset.sfo")
+synth.program_select(0, sfid, 0, 2) #select instrument type
+synth.start()
+notes = [48, 50, 52, 53]
+
+class handDetector():
+  def __init__(self, mode = False, maxHands = 2, modelComplex = 1,detectionCon = .5, trackCon = .5):
+    self.mode = mode
+    self.maxHands = maxHands
+    self.detectionCon = detectionCon
+    self.trackCon = trackCon
+    self.modelComplex = modelComplex
+
+    self.mpHands =  mp.solutions.hands
+    self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplex, self.detectionCon, self.trackCon)
+    self.mpDraw = mp.solutions.drawing_utils
+  
+  def findHands(self, img, draw = True):
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    self.results = self.hands.process(imgRGB)
+
+    if self.results.multi_hand_landmarks:
+      for handLms in self.results.multi_hand_landmarks:
+        if draw:
+          self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+    return img
+  def findPosition(self, img, handNo = 0, draw =  True):
+    lmlist = []
+    if self.results.multi_hand_landmarks:
+      myHand = self.results.multi_hand_landmarks[handNo]
+      for id, lm in enumerate(myHand.landmark):
+        h, w, cc = img.shape
+        cx, cy = int(lm.x * w), int(lm.y * h)
+        lmlist.append([id, cx, cy])
+        if draw: 
+          cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+    return lmlist
+
+def dist(point1, point2):
+  return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)**0.5
+
+def compute(vec):
+  if (vec[0] == []):
+    return []
+  base_scale = dist(vec[17], vec[5])
+  fingers = [dist(vec[4], vec[8]), dist(vec[4], vec[12]), dist(vec[4], vec[16]), dist(vec[4], vec[20])]
+  ratios = [fingers[i] / base_scale for i in range(len(fingers))]
+  return ratios
+
+def computeDiff(ratio1, ratio2):
+  res = 0
+  for i in range(len(ratio1)):
+    res += (ratio1[i] - ratio2[i])**2
+  return res**0.5
+
+def convert(list):
+  vec = [[] for i in range(21)]
+  for x in list:
+    vec[x[0]] = (x[1], x[2])
+  return vec
+
+def right(vec):
+    inds = [4, 8, 12, 16, 20]
+    gre = 0
+    for i in range(len(inds) - 1):
+        if (vec[inds[i]][0] < vec[inds[i+1]][0]):
+            gre += 1
+    if (gre <= 2):
+        return 0
+    else:
+        return 1
+
+def main():
+  pTime = 0
+  cTime = 0
+  cap = cv2.VideoCapture(0)
+  detector = handDetector()
+
+  #CALIBRATION
+  
+  count = 0
+  last = []
+  inconsistency = 0
+  ar_valid = [[],[]]
+  while (len(ar_valid[0]) <= 20 or len(ar_valid[1]) <= 20):
+    success, img = cap.read()
+    cv2.putText(img, "PLACE YOUR HANDS OUT IN FRONT OF YOU FOR CALIBRATION", (200,500), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    img = detector.findHands(img)
+    lmlist = detector.findPosition(img)
+    print(len(ar_valid))
+    
+    if (last != []):
+      # print(computeDiff(compute(convert(lmlist)), last))
+      # print(compute(convert(lmlist)))
+      if (len(ar_valid)[right(convert(lmlist))] == 0):
+        ar_valid[right(convert(lmlist))].append(compute(convert(lmlist)))
+      elif (computeDiff(compute(convert(lmlist)), ar_valid[right(convert(lmlist))][0]) > 0.1):
+          inconsistency += 1
+          cv2.putText(img, "DON'T MOVE YOUR HANDS!!", (200,600), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+      else:
+        ar_valid[right(convert(lmlist))].append(compute(convert(lmlist)))
+    if (inconsistency >= 20):
+      ar_valid[right(convert(lmlist))] = []
+      inconsistency = 0
+    # if len(lmlist) != 0:
+    #   print(lmlist[4])
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+  defaultratios = [[0 for i in range(len(ar_valid[0]))] for i in range(2)]
+  print(defaultratios)
+  for k in range(2):
+    for i in range(len(ar_valid[k][0])):
+        for j in range(len(ar_valid[k])):
+            defaultratios[k][i] += ar_valid[k][j][i]
+            defaultratios[k][i] /= len(ar_valid[k])
+  last_fingers = None
+  while (True):
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmlist = detector.findPosition(img)
+    
+    if (compute(convert(lmlist)) != []):
+      currentratios = compute(convert(lmlist))
+      print(currentratios)
+      fingers = []
+      flag = False
+      for i in range(len(currentratios)):
+        if currentratios[i]/defaultratios[right(convert(lmlist))][i] < 0.3:
+          fingers.append(i)
+      print(fingers)
+      if (last_fingers != None):
+        for finger in fingers:
+            if (finger not in last_fingers[right(convert(lmlist))]):
+                synth.noteon(0, notes[finger], 100)
+                print("noteon:", finger)
+        for finger in last_fingers[right(convert(lmlist))]:
+            if (finger not in fingers):
+                synth.noteoff(0, notes[finger])
+                print("noteoff:", finger)
+      if (last_fingers == None):
+        last_fingers = [[],[]]
+      last_fingers[right(convert(lmlist))] = fingers
+
+      cv2.putText(img, str(fingers), (200,500), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    # if len(lmlist) != 0:
+    #   print(lmlist[4])
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+if __name__ == "__main__":
+  main()
+
+
+
+
